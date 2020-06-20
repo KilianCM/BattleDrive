b1565253bac14982f8e31324b6c1e0979c708997
kchamiot <chamiot.k@gmail.com>
2020-06-20 10:50:17 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App';
+import VueRouter from 'vue-router';
+import HomePage from "./components/HomePage";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+const routes = [
+  { path: '/', component: HomePage }
+  ];
+
+const router = new VueRouter({
+  routes
+});
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
