6b8bcbb059f4609af89ab97738813389c95db0aa
kchamiot <chamiot.k@gmail.com>
2020-08-12 10:58:29 +0200

    Improve SEO

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const path = require('path'); 
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+
 module.exports = {
     css: {
         loaderOptions: {
@@ -10,5 +13,14 @@ module.exports = {
                 `
             }
         }
+    },
+    configureWebpack: {
+        plugins: [
+          new PrerenderSPAPlugin({
+            staticDir: path.join(__dirname, 'dist'),
+            // Required - Routes to render.
+            routes: [ '/', '/qui-sommes-nous', '/nos-partenaires', '/notre-objectif' ],
+          })
+        ]
     }
 };
